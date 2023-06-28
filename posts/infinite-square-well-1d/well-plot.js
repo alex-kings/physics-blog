a1506912a18590d9a088928e476902d2d89347da
alex-kings <alexandrekings1@gmail.com>
2023-06-28 18:38:08 +0100

    Change expression depending on amplitudes chosen.

--- posts/infinite-square-well-1d/well-plot.js
+++ posts/infinite-square-well-1d/well-plot.js
@@ -57,14 +57,70 @@ graph2.setFunc((x)=>{
 graph2.setIndicators([new Indicator(a, "L")],[])
 graph2.redraw();
 
+// Amplitude values
+const aTot = document.getElementById("aTot");
+const a1 = document.getElementById("a1");
+const a2 = document.getElementById("a2");
+const a3 = document.getElementById("a3");
+const graphExpr = document.getElementById("graph-expression");
+const invalidExpr = document.getElementById("invalid-expression");
+
+// Set the normalizes amplitude
+function setAtot() {
+    if(sliderA1.value === "0" && sliderA2.value === "0" && sliderA3.value === "0"){
+        invalidExpr.style.display = "block";
+        graphExpr.style.display = "none";
+    }
+    else {
+        invalidExpr.style.display = "none";
+        graphExpr.style.display = "block";
+    }
+    let s = Math.sqrt(parseFloat(sliderA1.value)**2 + parseFloat(sliderA2.value)**2 + parseFloat(sliderA3.value)**2);
+    s = 1/s;
+    aTot.innerHTML = Number(s).toFixed(2);
+}
+function setA1() {
+    let amplitude = sliderA1.value;
+    if(amplitude === "1") amplitude = "";
+    else if(amplitude === "-1") amplitude = "-";
+    a1.innerHTML = amplitude;
+}
+function setA2() {
+    let amplitude = sliderA2.value;
+    if(amplitude === "1") amplitude = "";
+    else if(amplitude === "-1") amplitude = "-";
+    a2.innerHTML = amplitude;
+}
+function setA3() {
+    let amplitude = sliderA3.value;
+    if(amplitude === "1") amplitude = "";
+    else if(amplitude === "-1") amplitude = "-";
+    a3.innerHTML = amplitude;
+}
+
+
+// Initialise values
+setA1();
+setA2();
+setA3();
+setAtot();
+
+// Initialise formula
 sliderA1.oninput = ()=>{ 
     graph2.redraw();
-
-    // Update maths
-    // document.getElementById("superposition-maths").innerHTML = "$$lambda$$";
+    setA1();
+    setAtot();
 }
-sliderA2.oninput = ()=>{ graph2.redraw();}
-sliderA3.oninput = ()=>{ graph2.redraw();}
+sliderA2.oninput = ()=>{ 
+    graph2.redraw();
+    setA2();
+    setAtot();
+}
+sliderA3.oninput = ()=>{ 
+    graph2.redraw();
+    setA3();
+    setAtot();
+}
 
 
 /**
