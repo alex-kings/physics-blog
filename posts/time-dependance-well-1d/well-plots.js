692dde63175501dc749c3a54f74e903dbc21c80a
alex-kings <alexandrekings1@gmail.com>
2023-07-21 22:36:35 +0100

    Start plot 2.

--- posts/time-dependance-well-1d/well-plots.js
+++ posts/time-dependance-well-1d/well-plots.js
@@ -32,3 +32,92 @@ graph.animate();
 sliderN.oninput = ()=>{
     nLabel.innerText = sliderN.value;
 }
+
+/**
+ * Plot 2
+ */
+
+const graph2 = new Graph1D("plot2");
+graph2.setLabelX("x");
+graph2.setLabelY("|Ψ|", -18);
+const sliderA1 = document.getElementById("sliderA1")
+const sliderA2 = document.getElementById("sliderA2")
+const sliderA3 = document.getElementById("sliderA3")
+graph2.setLimits(-0.2, 3.4, -1.5, 2);
+graph2.setSteps(600);
+let n1 = 1;
+let n2 = 2;
+let n3 = 3;
+let a = 3;
+graph2.addFunc(new Function((x)=>{
+    let a1 = parseFloat(sliderA1.value);
+    let a2 = parseFloat(sliderA2.value);
+    let a3 = parseFloat(sliderA3.value);
+    let A = 1/Math.sqrt(a1**2 + a2**2 + a3**2);
+    if(x < 0 || x > a) return null;
+    return A*(a1*Math.sin(n1*Math.PI*x/a) + a2*Math.sin(n2*Math.PI*x/a) + a3*Math.sin(n3*Math.PI*x/a))
+}))
+graph2.setIndicators([new Indicator(a, "L")],[])
+// Animate the plot
+graph2.animate();
+
+
+const aTot = document.getElementById("aTot");
+const a1 = document.getElementById("a1");
+const a2 = document.getElementById("a2");
+const a3 = document.getElementById("a3");
+const graphExpr = document.getElementById("graph-expression");
+const invalidExpr = document.getElementById("invalid-expression");
+
+// Set the normalizes amplitude
+function setAtot() {
+    if(sliderA1.value === "0" && sliderA2.value === "0" && sliderA3.value === "0"){
+        invalidExpr.style.display = "block";
+        graphExpr.style.display = "none";
+    }
+    else {
+        invalidExpr.style.display = "none";
+        graphExpr.style.display = "block";
+    }
+    let s = Math.sqrt(parseFloat(sliderA1.value)**2 + parseFloat(sliderA2.value)**2 + parseFloat(sliderA3.value)**2);
+    s = 1/s;
+    aTot.innerHTML = Number(s).toFixed(2);
+}
+function setA1() {
+    let amplitude = sliderA1.value;
+    if(amplitude === "1") amplitude = "";
+    else if(amplitude === "-1") amplitude = "-";
+    a1.innerHTML = amplitude;
+}
+function setA2() {
+    let amplitude = sliderA2.value;
+    if(amplitude === "1") amplitude = "";
+    else if(amplitude === "-1") amplitude = "-";
+    a2.innerHTML = amplitude;
+}
+function setA3() {
+    let amplitude = sliderA3.value;
+    if(amplitude === "1") amplitude = "";
+    else if(amplitude === "-1") amplitude = "-";
+    a3.innerHTML = amplitude;
+}
+
+// Initialise values
+setA1();
+setA2();
+setA3();
+setAtot();
+
+// Initialise formula
+sliderA1.oninput = ()=>{ 
+    setA1();
+    setAtot();
+}
+sliderA2.oninput = ()=>{ 
+    setA2();
+    setAtot();
+}
+sliderA3.oninput = ()=>{ 
+    setA3();
+    setAtot();
+}
