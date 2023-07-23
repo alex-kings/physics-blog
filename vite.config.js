e8dbb3d7329fe10ac12a5c7732188313a4f7ea2e
alex-kings <alexandrekings1@gmail.com>
2023-07-23 20:52:27 +0100

    Add vite config file.

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about/index.html'),
+        contact: resolve(__dirname, 'contact/index.html'),
+        post1: resolve(__dirname, 'posts/infinite-square-well-1d/index.html'),
+        post1: resolve(__dirname, 'posts/time-dependance-well-1d/index.html'),
+      },
+    },
+  },
+})
