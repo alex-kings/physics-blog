19e2982dbeaea23bb1d26b7383617bc9a60d9b3a
alex-kings <alexandrekings1@gmail.com>
2023-07-31 23:04:23 +0200

    Test remove ABOUT page.

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        about: resolve(__dirname, 'about/index.html'),
+        // about: resolve(__dirname, 'about/index.html'),
         contact: resolve(__dirname, 'contact/index.html'),
         post1: resolve(__dirname, 'posts/infinite-square-well-1d/index.html'),
         post2: resolve(__dirname, 'posts/time-dependance-well-1d/index.html'),
