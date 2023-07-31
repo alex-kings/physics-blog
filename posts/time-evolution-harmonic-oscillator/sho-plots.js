885a53f96deee6b563ae7efbf5e8465a7c519109
alex-kings <alexandrekings1@gmail.com>
2023-07-31 22:58:30 +0200

    Revert to previous working build.

--- posts/time-evolution-harmonic-oscillator/sho-plots.js
+++
@@ -1,9 +0,0 @@
-import {Graph1D, Indicator, Function} from "../../scripts/graphing/Graph1D.js";
-
-// Eigenfunctions
-
-
-// Eigenfunctions time-dependance plot
-const graph1 = new Graph1D("plot1");
-
-
--- python-graphs/graph.py
+++ python-graphs/graph.py
@@ -6,39 +6,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 
-imgName = "harmonic-oscillator-potential"
+imgName = "well-potential"
 folder = "./graphs/"
 format = ".jpg"
  
 # specifying the plot size
 plt.figure(figsize = (10, 5))
 
-# # Remove top and right borders
-# for pos in ['right', 'top']:
-#     plt.gca().spines[pos].set_visible(False)
+# Remove top and right borders
+for pos in ['right', 'top']:
+    plt.gca().spines[pos].set_visible(False)
 
     
-# # Reposition y axis
-# plt.gca().spines['left'].set_position(('data', 0))
+# Reposition y axis
+plt.gca().spines['left'].set_position(('data', 0))
 
-# # Remove ticks
-# plt.tick_params(left = False, right = False , labelleft = False ,
-#                 labelbottom = False, bottom = False)
+# Remove ticks
+plt.tick_params(left = False, right = False , labelleft = False ,
+                labelbottom = False, bottom = False)
 
 # Limits
-# plt.xlim(-3, 8)
+plt.xlim(-3, 8)
  
 # only one line may be specified; full height
-# plt.axvline(x = 5, color = 'black', label = 'well end')
+plt.axvline(x = 5, color = 'black', label = 'well end')
  
-# Create function
-x = np.linspace(-10,10,100)
-y = x*x
-
-plt.plot(x,y, label="V")
-
-plt.legend()
-
 # rendering plot
 plt.savefig(folder + imgName + format)
 im = Image.open(folder + imgName + format).convert("RGB")
