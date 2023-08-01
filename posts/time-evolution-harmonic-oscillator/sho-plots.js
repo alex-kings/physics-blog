7ba199a373c11e9d7aa230f2877b0c0ec413cfb5
alex-kings <alexandrekings1@gmail.com>
2023-08-01 21:51:28 +0200

    Fix ladder operator formula.

--- posts/time-evolution-harmonic-oscillator/sho-plots.js
+++ posts/time-evolution-harmonic-oscillator/sho-plots.js
@@ -1,9 +1,15 @@
-// import {Graph1D, Indicator, Function} from "../../scripts/graphing/Graph1D.js";
-
-// Eigenfunctions
+import {Graph1D, Indicator, Function} from "../../scripts/graphing/Graph1D.js";
 
 
 // Eigenfunctions time-dependance plot
-// const graph1 = new Graph1D("plot1");
+const graph1 = new Graph1D("plot1");
+const sliderN = document.getElementById("sliderN");
 
+const eigenfunction = () => {
+    const n = parseInt(sliderN.value);
+    
+    for(let i = 0; i < n; i++) {
+
+    }
+}
 
