d9397e2ad445fb94addbd4269e48fc903bfb6acd
alex-kings <alexandrekings1@gmail.com>
2023-08-03 10:56:10 +0200

    Rebuild with KATEX in first post.

--- posts/infinite-square-well-1d/well-plot.js
+++ posts/infinite-square-well-1d/well-plot.js
@@ -144,25 +144,90 @@ function displayLabels() {
 
 
 // Initialise values
-displayLabels();
-setAtot();
+// displayLabels();
+// setAtot();
 
 // Initialise formula
 sliderA1.oninput = ()=>{ 
     graph2.redraw();
-    displayLabels();
-    setAtot();
+    // displayLabels();
+    // setAtot();
+    drawExpression();
 }
 sliderA2.oninput = ()=>{ 
     graph2.redraw();
-    displayLabels();
-    setAtot();
+    // displayLabels();
+    // setAtot();
+    drawExpression();
 }
 sliderA3.oninput = ()=>{ 
     graph2.redraw();
-    displayLabels();
-    setAtot();
+    // displayLabels();
+    // setAtot();
+    drawExpression();
 }
+
+function drawExpression() {
+    const amp1 = parseFloat(sliderA1.value);
+    const amp2 = parseFloat(sliderA2.value);
+    const amp3 = parseFloat(sliderA3.value);
+
+    const container = document.getElementById("graph-expression");
+    katex.render(`\\psi=A\\left(${amp1}\\psi_1+${amp2}\\psi_2+${amp3}\\psi_3\\right)`,container,{
+        throwOnError:false,
+        displayMode:true
+    })
+
+    // Display expressions
+    if(amp1 === 0) expr1.style.display = "none";
+    else expr1.style.display = "block";
+    if(amp2 === 0) expr2.style.display = "none";
+    else expr2.style.display = "block";
+    if(amp3 === 0) expr3.style.display = "none";
+    else expr3.style.display = "block";
+
+    // Display sign of "plus" signs
+    if(amp2 === 0) plus1.innerHTML = (amp3 < 0 ? "−" : "+");
+    else plus1.innerHTML = (amp2 < 0 ? "−" : "+");
+    plus2.innerHTML = (amp3 < 0 ? "−" : "+");
+
+    // Display "plus" signs
+    if(amp1 === 0) plus1.style.display = "none";
+    else plus1.style.display = "block";
+    if(amp2 === 0) plus2.style.display = "none";
+    else plus2.style.display = "block";
+    if(amp3 === 0) {
+        if(amp2 === 0) plus1.style.display = "none";
+        else plus2.style.display = "none";
+    }
+
+    // Display amplitudes
+    // a1
+    if(amp1 === 1) a1.innerHTML = "";
+    else if(amp1 === -1) a1.innerHTML = "-";
+    else a1.innerHTML = amp1;
+    // a2
+    if(amp1 === 0) {
+        if(amp2 === 1) a2.innerHTML = "";
+        else if (amp2 === -1) a2.innerHTML = "-";
+        else a2.innerHTML = amp2;
+    }
+    else {
+        if(amp2 === 1 || amp2 === -1) a2.innerHTML = ""; 
+        else a2.innerHTML = Math.abs(amp2);
+    }
+    // a3
+    if(amp2 === 0 && amp1 === 0) {
+        if(amp3 === 1) a3.innerHTML = "";
+        else if(amp3 === -1) a3.innerHTML = "-";
+        else a3.innerHTML = amp3;
+    }
+    else {
+        if(amp3 === 1 || amp3 === -1) a3.innerHTML = "";
+        else a3.innerHTML = Math.abs(amp3);
+    }
+}
+drawExpression();
 
 
 /**
