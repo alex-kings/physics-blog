adfe270f9f5ccd9aed30de1199a9230a41d5fe70
Alexandre Kings <alexandrekings1@gmail.com>
2024-01-21 21:11:32 +0100

    Continue with poisson demo.

--- interactive/poisson-solver/poisson-solver.js
+++ interactive/poisson-solver/poisson-solver.js
@@ -1,13 +1,61 @@
+import * as THREE from 'three';
+
+// Set scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xAAAAAA);
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+const material = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
+
+// Lighting
+const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( light );
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.8 );
+directionalLight.position.set(-20,20,-20);
+directionalLight.lookAt(new THREE.Vector3(0,0,0))
+scene.add( directionalLight );
+
+camera.position.z = 15;
+camera.position.y = 7;
+camera.position.x = 3;
+camera.lookAt(new THREE.Vector3(0,0,0))
+
+
 class Grid {
     grid = [];
     sources = new Set();
-    n;
-    constructor(n, sources) {
-        this.n=n;
-        for(let i = 0; i < n; i++) this.grid.push(Array(n).fill(0));
+    n = 30;
+    L = 10;
+    geometry = new THREE.BoxGeometry( this.L/this.n, this.L/this.n, this.L/this.n );
+
+    // Rectangles contains threejs objects
+    rectangles = [];
+    constructor(sources) {
+        for(let i = 0; i < this.n; i++) {
+            this.grid.push(Array(this.n).fill(0));
+            this.rectangles.push([]);
+        }
         for(let source of sources) {
             this.grid[source[0]][source[1]] = 10;
             this.sources.add(JSON.stringify(source));
+        }
+        this.createRectangles();
+    }
+
+    createRectangles() {
+        for(let i = 0; i < this.n-1; i++) {
+            for(let j = 0; j < this.n-1; j++) {
+                const rectangle = new THREE.Mesh(this.geometry, material);
+                rectangle.position.set(
+                    (i*this.L)/this.n - this.L/2,
+                    this.grid[i][j] - this.L/2,
+                    (j*this.L)/this.n - this.L/2);
+                this.rectangles[i][j] = rectangle;
+                scene.add(rectangle);
+            }
         }
     }
 
@@ -31,9 +79,26 @@ class Grid {
     solve() {
         console.log(this.grid);   
     }
+    updateRectangles() {
+        for(let i = 0; i < this.n-1; i++) {
+            for(let j = 0; j < this.n-1; j++) {
+                this.rectangles[i][j].position.y = this.grid[i][j] - this.L/2;
+            }
+        }
+    }   
 }
 
-let myGrid = new Grid(10, [[3,3]]);
+function animate() {
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+}
+
+
+let myGrid = new Grid([[3,3],[13,13]]);
 myGrid.solve();
-for(let i = 0; i < 50; i++) myGrid.gaussSeidelStep();
+for(let i = 0; i < 40; i++) myGrid.gaussSeidelStep();
 myGrid.solve();
+
+myGrid.updateRectangles();
+
+animate();
