3d0cba426859e1d0ac53b68211068bdfd364742a
Alexandre Kings <alexandrekings1@gmail.com>
2024-02-25 13:42:55 +0100

    Fix global factor issues.

--- tools/unitary-matrix-verifier/unitary-matrix.js
+++ tools/unitary-matrix-verifier/unitary-matrix.js
@@ -14,11 +14,17 @@ const dimInput = document.getElementById("dimension-input");
 const dimBtn = document.getElementById("dimension-btn");
 const checkBtn = document.getElementById("check-btn");
 const globalFactorInput = document.getElementById("global-factor-input");
+const msgOutput = document.getElementById("msg-output");
+const msgSuccess = document.getElementById("msg-success");
+const msgFail = document.getElementById("msg-fail");
 let dim = 3;
+let globalFactor = 1;
 
-// CHANGE OF DIMENSIONS
+// CHANGE OF DIMENSIONS & GLOBAL FACTOR
 dimBtn.addEventListener("click",()=>{
     dim = parseInt(dimInput.value);
+    globalFactor = evaluate(globalFactorInput.value);
+    if(globalFactor === undefined) globalFactor = 1;
     if(dim && dim > 1 && dim < 10) changeDimensions();
 })
 function changeDimensions() {
@@ -39,15 +45,18 @@ document.addEventListener("keydown",(event)=>{
 
 // Verify if matrix is unitary
 checkBtn.addEventListener("click",()=>{
-    let [matA, matB] = getMatrices();
+    // Reset visibility
+    msgOutput.style.visibility = "hidden";
+    msgFail.style.display = "none";
+    msgSuccess.style.display = "none";
 
-    console.log(matA.toTex())
+    let result = checkUnitary();
 
-    let globalFactor = evaluate(globalFactorInput.value);
-    
-    let matC = multiply(matA, matB);
-    matC = multiply(matC, globalFactor * globalFactor);
-    console.log(checkIdentity(matC));
+    // Show output
+    window.scrollBy({top:10, behavior:"smooth"});
+    msgOutput.style.visibility = "visible";
+    if(result) msgSuccess.style.display = "inline-block";
+    else msgFail.style.display = "inline-block";
 })
 
 // Check if the given matrix is the identity
@@ -82,4 +91,17 @@ function getMatrices() {
         }
     }  
     return [matrix(matA),matrix(matB)];
+}
+
+// Return whether the input matrix is unitary
+function checkUnitary() {
+    let mats = getMatrices();
+    if(!mats) return false;
+    let [matA, matB] = mats;
+
+    // console.log(matA.toTex())
+    
+    let matC = multiply(matA, matB);
+    matC = multiply(matC, globalFactor * globalFactor);
+    return(checkIdentity(matC));
 }
